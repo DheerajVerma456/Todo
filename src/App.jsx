7692fc96e3a97daa12811a4ca3229cee0bfd3769
Dheeraj Verma <dheerajverma55569@gmail.com>
2023-05-30 00:40:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import { Toaster } from "react-hot-toast";
+import { useContext, useEffect } from "react";
+import axios from "axios";
+import { Context, server } from "./main";
+
+function App() {
+  const { setUser, setIsAuthenticated, setLoading } = useContext(Context);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`${server}/users/me`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        setUser(res.data.user);
+        setIsAuthenticated(true);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setUser({});
+        setIsAuthenticated(false);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+      </Routes>
+      <Toaster />
+    </Router>
+  );
+}
+
+export default App;
